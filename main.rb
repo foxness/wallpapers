3dc432f15c8f22a3c8638732e0c8d8949a389674
foxneZz <foxnezz@gmail.com>
2017-04-08 01:52:20 +0300

    overhauled

--- main.rb
+++ main.rb
@@ -1,19 +1,33 @@
 require 'open-uri'
 
 RAW_SOURCE_URL = "https://www.reddit.com/r/wallpapers/top/.json?sort=top&t=day"
-SLIDESHOW_DIR = "$HOME/wallpaper/"
+SLIDESHOW_DIR = File.expand_path "~/wallpaper"
+SLIDESHOW_DELAY = 60 # seconds
 
-WALLPAPER_FILENAME = "wallpaper"
-TEMP_FILENAME = "tmp"
+# -----------------------
 
-raw = open(RAW_SOURCE_URL, &:read)
-wallpapers = raw.scan(/\"permalink\": \"(?<permalink>[^\"]+)\".+?\"url\": \"(?<url>[^\"]+)\"/)
+current_wallpaper = Dir.glob(File.join SLIDESHOW_DIR, '*') \
+                    .map { |path| File.basename path }
+                    .select { |name| name[1] == '.' } \
+                    .first
 
+fail 'no current wallpaper found' if current_wallpaper.nil?
+
+wallpapers = open(RAW_SOURCE_URL, &:read).scan /\"permalink\": \"(?<permalink>[^\"]+)\".+?\"url\": \"(?<url>[^\"]+)\"/
 url = wallpapers[0][1]
-filename = "#{WALLPAPER_FILENAME}.#{url.split('.')[-1]}"
 
-command = "wget -q -O \"#{SLIDESHOW_DIR}#{TEMP_FILENAME}\" #{url}"\
-      " && rm -f \"#{SLIDESHOW_DIR}#{WALLPAPER_FILENAME}.*\""\
-      " && mv \"#{SLIDESHOW_DIR}#{TEMP_FILENAME}\" \"#{SLIDESHOW_DIR}#{filename}\""
+tick = current_wallpaper.nil? ? '2' : current_wallpaper[0]
+tock = (tick.to_i % 2 + 1).to_s
 
-`#{command}`
+pathify = lambda { |path| "\"#{File.join SLIDESHOW_DIR, path}\"" }
+
+temp = pathify['tmp']
+new_wallpaper = pathify["#{tock}.#{url.split('.')[-1]}"]
+current_wallpaper = pathify[current_wallpaper]
+
+cmds = "wget -q -O #{temp} #{url} " \
+    "&& mv #{temp} #{new_wallpaper} " \
+    "&& sleep #{SLIDESHOW_DELAY + 3} " \
+    "&& rm -f #{current_wallpaper}"
+
+`#{cmds}`
